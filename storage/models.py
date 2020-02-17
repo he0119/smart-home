e7b857b2fc60fb16017c573894705b0d121c396a
hemengyang <hmy0119@gmail.com>
2020-02-17 17:59:28 +0800

    允许父节点和描述为空

--- storage/models.py
+++ storage/models.py
@@ -4,9 +4,14 @@ from django.db import models
 class Storage(models.Model):
     """ 存储空间 """
     name = models.CharField(max_length=30)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE)
-    description = models.CharField(max_length=200)
+    parent = models.ForeignKey('self',
+                               on_delete=models.CASCADE,
+                               null=True,
+                               blank=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
 
+    def __str__(self):
+        return self.name
 
 
 class Item(models.Model):
@@ -15,3 +20,6 @@ class Item(models.Model):
     number = models.IntegerField()
     storage = models.ForeignKey(Storage, on_delete=models.CASCADE)
     update_date = models.DateTimeField('date updated')
+
+    def __str__(self):
+        return f'{self.name} in {self.storage.name}'
