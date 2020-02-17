c8dea8b52f94478f476817f3be4b163bd2aca9b3
hemengyang <hmy0119@gmail.com>
2020-02-17 18:54:02 +0800

    调整 URL

--- home/urls.py
+++ home/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('storage/', include('storage.urls')),
+    path('', include('storage.urls')),
     path('admin/', admin.site.urls),
 ]
--- storage/urls.py
+++ storage/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'storage'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:storage_id>/', views.storage_detail, name='storage_detail'),
+    path('storage/<int:storage_id>/', views.storage_detail, name='storage_detail'),
     path('item/<int:item_id>/', views.item_detail, name='item_detail'),
 ]
