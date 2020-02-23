6d673f4d2a083383e21a542c41f700d2aaf0f9c6
hemengyang <hmy0119@gmail.com>
2020-02-23 14:15:03 +0800

    切换到 PostgreSQL

--- home/settings/production.py
+++ home/settings/production.py
@@ -5,3 +5,14 @@ import os
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
 ALLOWED_HOSTS = '*'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    }
+}
