e1a95cda8c82561aad580f13f6512be5817ed77b
hemengyang <hmy0119@gmail.com>
2020-02-25 17:56:31 +0800

    完成部署清单 (#15)
    * 启用了一些安全设置
    * 使用腾讯企业邮箱
    * 添加备份与还原的说明

--- home/settings/production.py
+++ home/settings/production.py
@@ -16,3 +16,25 @@ DATABASES = {
         'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
+
+# Security
+# https://docs.djangoproject.com/en/3.0/topics/security/
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_REFERRER_POLICY = 'same-origin'
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+
+# Email
+# https://docs.djangoproject.com/zh-hans/3.0/topics/email/
+
+EMAIL_HOST = 'smtp.exmail.qq.com'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+SERVER_EMAIL = os.getenv('EMAIL_HOST_USER')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
