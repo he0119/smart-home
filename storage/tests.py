24ae03fc3f3396c4f284c0cf788deb58aab83811
hemengyang <hmy0119@gmail.com>
2020-03-14 10:46:04 +0800

    通过固定内容提供数据

--- storage/tests.py
+++ storage/tests.py
@@ -7,18 +7,11 @@ from .models import Storage
 
 def query_set_to_list(query_set):
     """ 转换 TreeQuerySet 到列表 """
-    new = []
-    for i in query_set:
-        new.append(i)
-    return new
+    return [i for i in query_set]
 
 
 class StorageModelTests(TestCase):
-    def setUp(self):
-        balcony = Storage.objects.create(name="阳台", description='家里的阳台')
-        locker = Storage.objects.create(name="阳台储物柜", parent=balcony)
-        toolbox = Storage.objects.create(name="工具箱", parent=locker)
-        toolbox2 = Storage.objects.create(name="工具箱2", parent=locker)
+    fixtures = ['tests.json']
 
     def test_ancestors(self):
         """ 测试父节点 """
@@ -29,21 +22,33 @@ class StorageModelTests(TestCase):
 
         self.assertEqual(balcony.parent, None)
         self.assertEqual(query_set_to_list(balcony.get_ancestors()), [])
-        self.assertEqual(query_set_to_list(balcony.get_children()), [locker])
 
         self.assertEqual(locker.parent, balcony)
         self.assertEqual(query_set_to_list(locker.get_ancestors()), [balcony])
-        self.assertEqual(query_set_to_list(locker.get_children()),
-                         [toolbox, toolbox2])
 
         self.assertEqual(toolbox.parent, locker)
         self.assertEqual(query_set_to_list(toolbox.get_ancestors()),
                          [balcony, locker])
 
+    def test_children(self):
+        balcony = Storage.objects.get(name="阳台")
+        locker = Storage.objects.get(name="阳台储物柜")
+        toolbox = Storage.objects.get(name="工具箱")
+        toolbox2 = Storage.objects.get(name="工具箱2")
+
+        self.assertEqual(query_set_to_list(balcony.get_children()), [locker])
+
+        self.assertEqual(query_set_to_list(locker.get_children()),
+                         [toolbox, toolbox2])
+
+        self.assertEqual(query_set_to_list(toolbox.get_children()), [])
+
 
 class UserTests(JSONWebTokenTestCase):
+    fixtures = ['tests.json']
+
     def setUp(self):
-        self.user = get_user_model().objects.create(username='test')
+        self.user = get_user_model().objects.get(username='test')
         self.client.authenticate(self.user)
 
     def test_get_user(self):
@@ -60,13 +65,11 @@ class UserTests(JSONWebTokenTestCase):
 
 
 class StoragesTests(JSONWebTokenTestCase):
+    fixtures = ['tests.json']
+
     def setUp(self):
-        self.user = get_user_model().objects.create(username='test')
+        self.user = get_user_model().objects.get(username='test')
         self.client.authenticate(self.user)
-        balcony = Storage.objects.create(name='阳台', description='家里的阳台')
-        locker = Storage.objects.create(name='阳台储物柜', parent=balcony)
-        toolbox = Storage.objects.create(name='工具箱', parent=locker)
-        toolbox2 = Storage.objects.create(name='工具箱2', parent=locker)
 
     def test_get_storages(self):
         query = '''
@@ -80,3 +83,18 @@ class StoragesTests(JSONWebTokenTestCase):
         self.assertIsNone(content.errors)
         names = [storage['name'] for storage in content.data['storages']]
         self.assertEqual(set(names), set(['阳台', '阳台储物柜', '工具箱', '工具箱2']))
+
+    def test_get_storage(self):
+        toolbox = Storage.objects.get(name='工具箱')
+
+        query = f'''
+            query storage {{
+                storage(id: {toolbox.id}) {{
+                    name
+                }}
+            }}
+        '''
+        content = self.client.execute(query)
+        self.assertIsNone(content.errors)
+        name = content.data['storage']['name']
+        self.assertEqual(name, toolbox.name)
