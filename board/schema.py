cbd7338c8666ceb04085e8e78c5db07ff5980d34
hemengyang <hmy0119@gmail.com>
2020-03-23 23:07:40 +0800

    完成话题相关的 mutation

--- board/schema.py
+++ board/schema.py
@@ -1,8 +1,5 @@
-from datetime import timedelta
-
 import graphene
 from django.contrib.auth import get_user_model
-from django.utils import timezone
 from graphene_django.types import DjangoObjectType
 from graphql.error import GraphQLError
 from graphql_jwt.decorators import login_required
@@ -10,6 +7,7 @@ from graphql_jwt.decorators import login_required
 from .models import Comment, Topic
 
 
+#region query
 class TopicType(DjangoObjectType):
     class Meta:
         model = Topic
@@ -37,14 +35,102 @@ class Query(graphene.ObjectType):
         return Topic.objects.get(pk=topic_id).comments.all()
 
 
-# class AddTopicMutation(graphene.Mutation):
-#     pass
+# endregion
 
-# class DeleteTopicMutation(graphene.Mutation):
-#     pass
 
-# class UpdateTopicMutation(graphene.Mutation):
-#     pass
+#region mutation
+class TopicInput(graphene.InputObjectType):
+    id = graphene.ID(required=True)
+    title = graphene.String()
+    description = graphene.String()
+
+
+class CommentInput(graphene.InputObjectType):
+    id = graphene.ID(required=True)
+    topic = TopicInput()
+    body = graphene.String()
+
+
+class AddTopicInput(graphene.InputObjectType):
+    title = graphene.String(required=True)
+    description = graphene.String()
+    parent = TopicInput()
+
+
+class UpdateTopicInput(graphene.InputObjectType):
+    id = graphene.ID(required=True)
+    title = graphene.String()
+    description = graphene.String()
+
+
+class AddCommentInput(graphene.InputObjectType):
+    topic = TopicInput(required=True)
+    body = graphene.String(required=True)
+    parent = CommentInput()
+
+
+class UpdateCommentInput(graphene.InputObjectType):
+    id = graphene.ID(required=True)
+    body = graphene.String()
+
+
+class AddTopicMutation(graphene.Mutation):
+    class Arguments:
+        input = AddTopicInput(required=True)
+
+    topic = graphene.Field(TopicType)
+
+    @login_required
+    def mutate(self, info, **kwargs):
+        input = kwargs.get('input')
+
+        topic = Topic(
+            title=input.title,
+            description=input.description,
+            user=info.context.user,
+        )
+        topic.save()
+        return AddTopicMutation(topic=topic)
+
+
+class DeleteTopicMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    deletedId = graphene.ID()
+
+    @login_required
+    def mutate(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        try:
+            storage = Topic.objects.get(pk=id)
+            storage.delete()
+            return DeleteTopicMutation(deletedId=id)
+        except Topic.DoesNotExist:
+            raise GraphQLError('话题不存在')
+
+
+class UpdateTopicMutation(graphene.Mutation):
+    class Arguments:
+        input = UpdateTopicInput(required=True)
+
+    topic = graphene.Field(TopicType)
+
+    @login_required
+    def mutate(self, info, **kwargs):
+        input = kwargs.get('input')
+
+        try:
+            topic = Topic.objects.get(pk=input.id)
+        except Topic.DoesNotExist:
+            raise GraphQLError('话题不存在')
+
+        topic.title = input.title
+        topic.description = input.description
+        topic.save()
+        return UpdateTopicMutation(topic=topic)
+
 
 # class AddCommentMutation(graphene.Mutation):
 #     pass
@@ -53,8 +139,13 @@ class Query(graphene.ObjectType):
 #     pass
 
 # class UpdateCommentMutation(graphene.Mutation):
-# pass
+#     pass
 
 
 class Mutation(graphene.ObjectType):
-    pass
+    add_topic = AddTopicMutation.Field()
+    delete_topic = DeleteTopicMutation.Field()
+    update_topic = UpdateTopicMutation.Field()
+
+
+#endregion
