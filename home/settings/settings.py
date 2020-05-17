9f7c1fcfd87d4f2d87a5742a3b06fb6a1b30cbb9
hemengyang <hmy0119@gmail.com>
2020-05-17 17:30:01 +0800

    调整本地开发环境数据库配置

--- home/settings/settings.py
+++ home/settings/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nu$#aq5e6x27t*j&hc4joq7y*wsvktqm-98oa=tqe6l+ch@hsq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.31.12']
 
 # Application definition
 
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'hehome.xyz',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
