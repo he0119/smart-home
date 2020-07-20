2132b8a236c932c52f65c7bc4ff9bbe386929f5a
hemengyang <hmy0119@gmail.com>
2020-07-20 19:32:51 +0800

    [IOT] 添加一次性设置多个参数的任务 Fixes #29

--- iot/tasks.py
+++ iot/tasks.py
@@ -1,11 +1,24 @@
+from typing import List, Tuple
+
 from celery import shared_task
 
 from .api import DeviceAPI
 
 
 @shared_task
-def set_status(device_id, key, value):
+def set_status(device_id: str, key: str, value):
     """ 设置设备参数 """
     if key is not None and value is not None:
         device_api = DeviceAPI(device_id)
         device_api.set_status(key, value)
+
+
+@shared_task
+def set_multiple_status(device_id: str, status: List[Tuple]):
+    """ 设置设备参数
+
+    status 为参数名和值的元组列表 [('valve1', True), ('valve2', True)]
+    """
+    if status is not None:
+        device_api = DeviceAPI(device_id)
+        device_api.set_multiple_status(status)
