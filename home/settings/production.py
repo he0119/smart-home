e31b083658489555b797684e51e1d3b440eca880
hemengyang <hmy0119@gmail.com>
2020-07-26 08:50:23 +0800

    修改默认数据库名称为 smart-home

--- home/settings/production.py
+++ home/settings/production.py
@@ -9,7 +9,7 @@ ALLOWED_HOSTS = '*'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_NAME'),
+        'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
