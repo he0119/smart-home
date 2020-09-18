2243dfc3c1cbee8c6a72120f61188e8ede6c487a
hemengyang <hmy0119@gmail.com>
2020-09-18 15:40:03 +0800

    test(iot): 添加测试用例

--- iot/tests.py
+++ iot/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from graphql_jwt.testcases import JSONWebTokenTestCase
 
-# Create your tests here.
+from .models import AutowateringData, Device
+
+
+class DeviceTests(JSONWebTokenTestCase):
+    fixtures = ['user', 'iot']
+
+    def setUp(self):
+        self.user = get_user_model().objects.get(username='test')
+        self.client.authenticate(self.user)
+
+    def test_get_devices(self):
+        """ 获取所有设备信息 """
+        query = '''
+            query devices {
+                devices {
+                    id
+                    name
+                }
+            }
+        '''
+        content = self.client.execute(query)
+        self.assertIsNone(content.errors)
+        names = [item['name'] for item in content.data['devices']]
+        self.assertEqual(set(names), set(['test']))
