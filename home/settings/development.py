b74f8314c37f0dc88912b12173f15e80940a7876
hemengyang <hmy0119@gmail.com>
2020-09-18 12:31:05 +0800

    feat(config): 从环境变量中读取开发环境数据库配置

--- home/settings/development.py
+++ home/settings/development.py
@@ -1,6 +1,22 @@
+""" 开发环境配置
+"""
+import os
+
 from .settings import INSTALLED_APPS, MIDDLEWARE
 
 ALLOWED_HOSTS = '*'
 INSTALLED_APPS.append('corsheaders')
 MIDDLEWARE = ['corsheaders.middleware.CorsMiddleware'] + MIDDLEWARE
 CORS_ORIGIN_ALLOW_ALL = True
+
+# 开发服务器配置，从环境变量里读取
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    }
+}
