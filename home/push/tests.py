def35b45f07b8505f3f9e9c4b8b900f9034ccfc6
hemengyang <hmy0119@gmail.com>
2020-12-02 21:42:58 +0800

    初步添加测试

--- home/push/tests.py
+++ home/push/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from graphql_jwt.testcases import JSONWebTokenTestCase
 
-# Create your tests here.
+from .models import MiPush
+
+
+class DeviceTests(JSONWebTokenTestCase):
+    fixtures = ['users', 'push']
+
+    def setUp(self):
+        self.user = get_user_model().objects.get(username='test')
+        self.client.authenticate(self.user)
+
+    def test_get_mipush(self):
+        """ 获取当前用户的注册标识码 """
+        query = '''
+            query miPush {
+                miPush {
+                    user {
+                        username
+                    }
+                    enable
+                    regId
+                }
+            }
+        '''
+        content = self.client.execute(query)
+        self.assertIsNone(content.errors)
+        self.assertEqual(content.data['miPush']['user']['username'], 'test')
+        self.assertEqual(content.data['miPush']['enable'], True)
+        self.assertEqual(content.data['miPush']['regId'], 'regidofuser1')
