de5c17f2cac1e07a8c3a691c1226086fa53f4450
hemengyang <hmy0119@gmail.com>
2020-12-02 21:03:08 +0800

    增加通知类别，并删除了广播

--- home/push/tasks.py
+++ home/push/tasks.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Literal, Optional
 from celery import shared_task
 from django.conf import settings
 
@@ -7,34 +7,49 @@ from .mipush.APISender import APISender
 
 sender = APISender(settings.MI_PUSH_APP_SECRET)
 
+CHANNELS = {
+    'iot': ['pre54', 'IoT消息'],
+    'storage': ['pre213', '物品管理信息'],
+    'board': ['pre84', '留言板消息'],
+}
 
-def build_message(title: str, description: str) -> PushMessage:
+
+def build_message(
+        title: str, description: str,
+        channel: Optional[Literal['iot', 'storage', 'board']]) -> PushMessage:
     # 每条内容相同的消息单独显示，不覆盖
-    notify_id = abs(hash(title + description)) % (10**8)
-    return PushMessage() \
+    # 限制最多可以有 10001 条消息共存
+    notify_id = abs(hash(title + description)) % (10**4)
+
+    message = PushMessage() \
         .restricted_package_name(settings.MI_PUSH_PACKAGE_NAME) \
         .title(title).description(description) \
-        .pass_through(0) \
         .notify_id(notify_id) \
-        .extra({Constants.extra_param_notify_effect: Constants.notify_launcher_activity, 'notification_style_type': '1' })
+        .extra({Constants.extra_param_notify_effect: Constants.notify_launcher_activity}) \
+        .extra({'notification_style_type': '1'})
+
+    # 通知类别
+    if channel:
+        message = message.extra(
+            {Constants.extra_param_channel_id: CHANNELS[channel][0]})
+        message = message.extra(
+            {Constants.extra_param_channel_name: CHANNELS[channel][1]})
+
+    return message
 
 
 @shared_task(max_retries=3, default_retry_delay=5)
-def push_to_user(reg_ids: str, title: str, description: str) -> None:
-    """ 向指定用户推送消息 """
-    message = build_message(title, description)
+def push_to_users(reg_ids: List[str],
+                  title: str,
+                  description: str,
+                  channel: Optional[Literal['iot', 'storage',
+                                            'board']] = None):
+    """ 向用户推送消息
+
+    支持向一个或多个用户推送消息
+
+    根据 regids, 发送消息到指定的一组设备上, regids 的个数不得超过 1000 个。
+    <https://dev.mi.com/console/doc/detail?pId=1278#_2_1>
+    """
+    message = build_message(title, description, channel)
     return sender.send(message.message_dict(), reg_ids)
-
-
-@shared_task(max_retries=3, default_retry_delay=5)
-def push_to_users(reg_ids: List[str], title: str, description: str) -> None:
-    """ 向多位用户推送消息 """
-    message = build_message(title, description)
-    return sender.send(message.message_dict(), reg_ids)
-
-
-@shared_task(max_retries=3, default_retry_delay=5)
-def push_to_all(title: str, description: str) -> None:
-    """ 向所有用户推送消息 """
-    message = build_message(title, description)
-    return sender.broadcast_all(message.message_dict())
