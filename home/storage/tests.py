9f7a4b8ab70d5a7849b199df010c170d9be93826
hemengyang <hmy0119@gmail.com>
2020-12-12 16:04:32 +0800

    test(storage): 完善测试

--- home/storage/tests.py
+++ home/storage/tests.py
@@ -6,9 +6,18 @@ from graphql_jwt.testcases import JSONWebTokenTestCase
 from .models import Item, Storage
 
 
-def query_set_to_list(query_set):
-    """ 转换 TreeQuerySet 到列表 """
-    return [i for i in query_set]
+class ModelTests(TestCase):
+    fixtures = ['users', 'storage']
+
+    def test_storage_str(self):
+        storage = Storage.objects.get(pk=1)
+
+        self.assertEqual(str(storage), '阳台')
+
+    def test_item_str(self):
+        item = Item.objects.get(pk=1)
+
+        self.assertEqual(str(item), '雨伞')
 
 
 class StorageModelTests(TestCase):
@@ -22,14 +31,13 @@ class StorageModelTests(TestCase):
         toolbox2 = Storage.objects.get(name="工具箱2")
 
         self.assertEqual(balcony.parent, None)
-        self.assertEqual(query_set_to_list(balcony.get_ancestors()), [])
+        self.assertEqual(list(balcony.get_ancestors()), [])
 
         self.assertEqual(locker.parent, balcony)
-        self.assertEqual(query_set_to_list(locker.get_ancestors()), [balcony])
+        self.assertEqual(list(locker.get_ancestors()), [balcony])
 
         self.assertEqual(toolbox.parent, locker)
-        self.assertEqual(query_set_to_list(toolbox.get_ancestors()),
-                         [balcony, locker])
+        self.assertEqual(list(toolbox.get_ancestors()), [balcony, locker])
 
     def test_children(self):
         balcony = Storage.objects.get(name="阳台")
@@ -37,12 +45,11 @@ class StorageModelTests(TestCase):
         toolbox = Storage.objects.get(name="工具箱")
         toolbox2 = Storage.objects.get(name="工具箱2")
 
-        self.assertEqual(query_set_to_list(balcony.get_children()), [locker])
+        self.assertEqual(list(balcony.get_children()), [locker])
 
-        self.assertEqual(query_set_to_list(locker.get_children()),
-                         [toolbox, toolbox2])
+        self.assertEqual(list(locker.get_children()), [toolbox, toolbox2])
 
-        self.assertEqual(query_set_to_list(toolbox.get_children()), [])
+        self.assertEqual(list(toolbox.get_children()), [])
 
 
 class StorageTests(JSONWebTokenTestCase):
@@ -81,6 +88,62 @@ class StorageTests(JSONWebTokenTestCase):
         name = content.data['storage']['name']
         self.assertEqual(name, toolbox.name)
 
+    def test_get_root_storage(self):
+        query = '''
+            query rootStorage  {
+                rootStorage {
+                    name
+                }
+            }
+        '''
+        content = self.client.execute(query)
+        self.assertIsNone(content.errors)
+
+        root_storage = content.data['rootStorage']
+        self.assertEqual(set([storage['name'] for storage in root_storage]),
+                         {'阳台'})
+
+    def test_get_storage_ancestors(self):
+        query = '''
+            query storageAncestors($id: ID!)  {
+                storageAncestors(id: $id) {
+                    name
+                }
+            }
+        '''
+        storage = Storage.objects.get(name='工具箱')
+        variables = {'id': storage.id}
+
+        content = self.client.execute(query, variables)
+        self.assertIsNone(content.errors)
+
+        storage_ancestors = content.data['storageAncestors']
+        self.assertEqual([storage['name'] for storage in storage_ancestors],
+                         ['阳台', '阳台储物柜', '工具箱'])
+
+    def test_search(self):
+        query = '''
+            query search($key: String!)  {
+                search(key: $key) {
+                    items {
+                        name
+                    }
+                    storages {
+                        name
+                    }
+                }
+            }
+        '''
+        variables = {'key': '口罩'}
+
+        content = self.client.execute(query, variables)
+        self.assertIsNone(content.errors)
+
+        items = content.data['search']['items']
+        storages = content.data['search']['storages']
+        self.assertEqual([item['name'] for item in items], ['口罩'])
+        self.assertEqual(storages, [])
+
     def test_add_storage(self):
         mutation = '''
             mutation addStorage($input: AddStorageInput!) {
@@ -90,6 +153,9 @@ class StorageTests(JSONWebTokenTestCase):
                         id
                         name
                         description
+                        parent {
+                            id
+                        }
                     }
                 }
             }
@@ -98,6 +164,9 @@ class StorageTests(JSONWebTokenTestCase):
             'input': {
                 'name': 'test',
                 'description': 'some',
+                'parent': {
+                    'id': 1
+                }
             }
         }
 
@@ -108,6 +177,7 @@ class StorageTests(JSONWebTokenTestCase):
         self.assertEqual(storage['__typename'], 'StorageType')
         self.assertEqual(storage['name'], 'test')
         self.assertEqual(storage['description'], 'some')
+        self.assertEqual(storage['parent']['id'], '1')
 
     def test_delete_storage(self):
         mutation = '''
@@ -141,6 +211,75 @@ class StorageTests(JSONWebTokenTestCase):
                         parent {
                             __typename
                             id
+                            name
+                        }
+                    }
+                }
+            }
+        '''
+        variables = {
+            'input': {
+                'id': 3,
+                'name': 'test',
+                'description': 'some',
+                'parent': {
+                    'id': 1
+                }
+            }
+        }
+
+        old_storage = Storage.objects.get(pk=3)
+        self.assertEqual(old_storage.name, '工具箱')
+
+        content = self.client.execute(mutation, variables)
+        self.assertIsNone(content.errors)
+
+        storage = content.data['updateStorage']['storage']
+        self.assertEqual(storage['__typename'], 'StorageType')
+        self.assertEqual(storage['id'], '3')
+        self.assertEqual(storage['name'], 'test')
+        self.assertEqual(storage['description'], 'some')
+        self.assertEqual(storage['parent']['id'], '1')
+        self.assertEqual(storage['parent']['name'], '阳台')
+
+    def test_add_storage_name_duplicate(self):
+        mutation = '''
+            mutation addStorage($input: AddStorageInput!) {
+                addStorage(input: $input) {
+                    storage {
+                        __typename
+                        id
+                        name
+                        description
+                    }
+                }
+            }
+        '''
+        variables = {
+            'input': {
+                'name': '阳台',
+                'description': 'some',
+            }
+        }
+
+        content = self.client.execute(mutation, variables)
+        self.assertIsNotNone(content.errors)
+
+        self.assertEqual(content.errors[0].message, '名称重复')
+
+    def test_update_storage_name_duplicate(self):
+        """ 测试修改名称重复 """
+        mutation = '''
+            mutation updateStorage($input: UpdateStorageInput!) {
+                updateStorage(input: $input) {
+                    storage {
+                        __typename
+                        id
+                        name
+                        description
+                        parent {
+                            __typename
+                            id
                         }
                     }
                 }
@@ -149,7 +288,7 @@ class StorageTests(JSONWebTokenTestCase):
         variables = {
             'input': {
                 'id': 1,
-                'name': 'test',
+                'name': '阳台储物柜',
                 'description': 'some',
             }
         }
@@ -158,13 +297,25 @@ class StorageTests(JSONWebTokenTestCase):
         self.assertEqual(old_storage.name, '阳台')
 
         content = self.client.execute(mutation, variables)
-        self.assertIsNone(content.errors)
-        storage = content.data['updateStorage']['storage']
+        self.assertIsNotNone(content.errors)
 
-        self.assertEqual(storage['__typename'], 'StorageType')
-        self.assertEqual(storage['id'], '1')
-        self.assertEqual(storage['name'], 'test')
-        self.assertEqual(storage['description'], 'some')
+        self.assertEqual(content.errors[0].message, '名称重复')
+
+    def test_delete_storage_not_exist(self):
+        mutation = '''
+            mutation deleteStorage($id: ID!) {
+                deleteStorage(id: $id) {
+                    deletedId
+                }
+            }
+        '''
+
+        variables = {'id': 0}
+
+        content = self.client.execute(mutation, variables)
+        self.assertIsNotNone(content.errors)
+
+        self.assertEqual(content.errors[0].message, '位置不存在')
 
 
 class ItemTests(JSONWebTokenTestCase):
@@ -173,19 +324,6 @@ class ItemTests(JSONWebTokenTestCase):
     def setUp(self):
         self.user = get_user_model().objects.get(username='test')
         self.client.authenticate(self.user)
-
-    def test_get_items(self):
-        query = '''
-            query items {
-                items {
-                    name
-                }
-            }
-        '''
-        content = self.client.execute(query)
-        self.assertIsNone(content.errors)
-        names = [item['name'] for item in content.data['items']]
-        self.assertEqual(set(names), set(['雨伞', '口罩']))
 
     def test_get_item(self):
         umbrella = Item.objects.get(name='雨伞')
@@ -202,6 +340,113 @@ class ItemTests(JSONWebTokenTestCase):
 
         name = content.data['item']['name']
         self.assertEqual(name, umbrella.name)
+
+    def test_get_items(self):
+        query = '''
+            query items {
+                items {
+                    name
+                }
+            }
+        '''
+        content = self.client.execute(query)
+        self.assertIsNone(content.errors)
+        names = [item['name'] for item in content.data['items']]
+        self.assertEqual(set(names), set(['雨伞', '口罩']))
+
+    def test_get_recently_updated_items(self):
+        query = '''
+            query recentlyUpdatedItems($number: Int!)  {
+                recentlyUpdatedItems(number: $number) {
+                    name
+                }
+            }
+        '''
+        variables = {'number': 5}
+
+        content = self.client.execute(query, variables)
+        self.assertIsNone(content.errors)
+
+        items = content.data['recentlyUpdatedItems']
+        self.assertEqual(set([item['name'] for item in items]), {'口罩', '雨伞'})
+
+    def test_get_recently_added_items(self):
+        query = '''
+            query recentlyAddedItems($number: Int!)  {
+                recentlyAddedItems(number: $number) {
+                    name
+                }
+            }
+        '''
+        variables = {'number': 5}
+
+        content = self.client.execute(query, variables)
+        self.assertIsNone(content.errors)
+
+        items = content.data['recentlyAddedItems']
+        self.assertEqual(set([item['name'] for item in items]), {'口罩', '雨伞'})
+
+    def test_get_near_expired_items(self):
+        query = '''
+            query nearExpiredItems($within: Int!, $number: Int)  {
+                nearExpiredItems(within: $within, number: $number) {
+                    name
+                }
+            }
+        '''
+        variables = {'within': 1}
+
+        content = self.client.execute(query, variables)
+        self.assertIsNone(content.errors)
+
+        items = content.data['nearExpiredItems']
+        self.assertEqual(items, [])
+
+    def test_get_near_expired_items_with_number(self):
+        query = '''
+            query nearExpiredItems($within: Int!, $number: Int)  {
+                nearExpiredItems(within: $within, number: $number) {
+                    name
+                }
+            }
+        '''
+        variables = {'within': 1, 'number': 2}
+
+        content = self.client.execute(query, variables)
+        self.assertIsNone(content.errors)
+
+        items = content.data['nearExpiredItems']
+        self.assertEqual(items, [])
+
+    def test_get_expired_items(self):
+        query = '''
+            query expiredItems($number: Int)  {
+                expiredItems(number: $number) {
+                    name
+                }
+            }
+        '''
+        content = self.client.execute(query)
+        self.assertIsNone(content.errors)
+
+        items = content.data['expiredItems']
+        self.assertEqual(items, [])
+
+    def test_get_expired_items_with_number(self):
+        query = '''
+            query expiredItems($number: Int)  {
+                expiredItems(number: $number) {
+                    name
+                }
+            }
+        '''
+        variables = {'number': 2}
+
+        content = self.client.execute(query, variables)
+        self.assertIsNone(content.errors)
+
+        items = content.data['expiredItems']
+        self.assertEqual(items, [])
 
     def test_add_item(self):
         mutation = '''
@@ -322,3 +567,101 @@ class ItemTests(JSONWebTokenTestCase):
         self.assertEqual(item['storage']['id'], '2')
         self.assertEqual(item['price'], 12.0)
         self.assertEqual(item['expirationDate'], expiration_date.isoformat())
+
+    def test_add_item_name_duplicate(self):
+        mutation = '''
+            mutation addItem($input: AddItemInput!) {
+                addItem(input: $input) {
+                    item {
+                        __typename
+                        id
+                        name
+                        number
+                        storage {
+                            id
+                            name
+                        }
+                        description
+                        price
+                        expirationDate
+                        updateDate
+                        editor {
+                            username
+                        }
+                    }
+                }
+            }
+        '''
+        variables = {
+            'input': {
+                'name': '雨伞',
+                'number': 1,
+                'storage': {
+                    'id': 1
+                },
+                'description': 'some',
+                'price': '12.0',
+                'expirationDate': None,
+            }
+        }
+
+        content = self.client.execute(mutation, variables)
+        self.assertIsNotNone(content.errors)
+
+        self.assertEqual(content.errors[0].message, '名称重复')
+
+    def test_update_item_name_duplicate(self):
+        mutation = '''
+            mutation updateItem($input: UpdateItemInput!) {
+                updateItem(input: $input) {
+                    item {
+                        __typename
+                        id
+                        name
+                        number
+                        storage {
+                            id
+                            name
+                        }
+                        description
+                        price
+                        expirationDate
+                        updateDate
+                        editor {
+                            username
+                        }
+                    }
+                }
+            }
+        '''
+        variables = {
+            'input': {
+                'id': 1,
+                'name': '口罩',
+                'number': 2,
+                'storage': {
+                    'id': 2
+                },
+            }
+        }
+
+        content = self.client.execute(mutation, variables)
+        self.assertIsNotNone(content.errors)
+
+        self.assertEqual(content.errors[0].message, '名称重复')
+
+    def test_delete_item_not_exist(self):
+        mutation = '''
+            mutation deleteItem($id: ID!) {
+                deleteItem(id: $id) {
+                    deletedId
+                }
+            }
+        '''
+
+        variables = {'id': 0}
+
+        content = self.client.execute(mutation, variables)
+        self.assertIsNotNone(content.errors)
+
+        self.assertEqual(content.errors[0].message, '物品不存在')
