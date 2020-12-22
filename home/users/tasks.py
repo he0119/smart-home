e13128a8224c5efb6f3a8d5b288d7caccb120b59
hemengyang <hmy0119@gmail.com>
2020-12-22 11:42:13 +0800

    fix(users): 修复清除令牌任务的条件错误

--- home/users/tasks.py
+++ home/users/tasks.py
@@ -5,4 +5,4 @@ from django.core import management
 @shared_task
 def clear_expired_tokens():
     """ 清楚过期的令牌 """
-    management.call_command('cleartokens', expired=False)
+    management.call_command('cleartokens', expired=True)
