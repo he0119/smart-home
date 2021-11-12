60916f8a6626347a6ad7b38f1752bbfaa41069c1
hemengyang <hmy0119@gmail.com>
2021-11-12 15:47:23 +0800

    添加 query

--- home/users/types.py
+++ home/users/types.py
@@ -1,10 +1,18 @@
+import graphene
 from django.contrib.auth import get_user_model
 from graphene import relay
 from graphene_django.types import DjangoObjectType
+from graphql_jwt.decorators import login_required
 
 
 class UserType(DjangoObjectType):
+    avatar = graphene.String()
+
     class Meta:
         model = get_user_model()
         fields = "__all__"
         interfaces = (relay.Node,)
+
+    @login_required
+    def resolve_avatar(self, info, **args):
+        return self.avatar.avatar.url
