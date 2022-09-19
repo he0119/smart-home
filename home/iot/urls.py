c5be51832a684e8078adfcb441253311df4d63b3
hemengyang <hmy0119@gmail.com>
2022-09-19 16:46:20 +0000

    支持设备上报数据，现在就可以把之前的删除掉

--- home/iot/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "iot"
-urlpatterns = [
-    path("", views.iot, name="iot"),
-]
--- home/iot/views.py
+++ home/iot/views.py
@@ -1,107 +1,18 @@
+import base64
 import json
 import logging
 from datetime import datetime
 
 import pytz
-from django.http import JsonResponse
+from channels.db import database_sync_to_async
+from channels.generic.websocket import WebsocketConsumer
 from django.utils import timezone
-from django.views.decorators.csrf import csrf_exempt
 
 from home.utils import channel_group_send
 
 from .models import AutowateringData, Device
 
 logger = logging.getLogger("iot")
-
-
-@csrf_exempt
-def iot(request):
-    """物联网
-
-    EMQX WebHook
-    """
-    if request.method == "POST":
-        event = json.loads(request.body)
-        logger.debug(event)
-        if event["action"] == "message_publish":
-            process_message_publish(event)
-        elif event["action"] == "client_connected":
-            process_client_connected(event)
-        elif event["action"] == "client_disconnected":
-            process_client_disconnected(event)
-        else:
-            logger.warning("未处理的事件")
-            logger.warning(event)
-
-    return JsonResponse({"iot": "working"})
-
-
-def process_message_publish(event):
-    """处理消息发布事件"""
-    device_name = event["from_username"]
-    topic = event["topic"]
-    if "status" in topic:
-        payload = json.loads(event["payload"])
-        try:
-            device: Device = Device.objects.get(name=device_name)
-            if device.device_type == "autowatering":
-                autowatering_data = AutowateringData(
-                    device=device,
-                    time=datetime.fromtimestamp(payload["timestamp"], pytz.utc),
-                    temperature=payload["data"]["temperature"],
-                    humidity=payload["data"]["humidity"],
-                    wifi_signal=payload["data"]["wifi_signal"],
-                    valve1=payload["data"]["valve1"],
-                    valve2=payload["data"]["valve2"],
-                    valve3=payload["data"]["valve3"],
-                    pump=payload["data"]["pump"],
-                    valve1_delay=payload["data"]["valve1_delay"],
-                    valve2_delay=payload["data"]["valve2_delay"],
-                    valve3_delay=payload["data"]["valve3_delay"],
-                    pump_delay=payload["data"]["pump_delay"],
-                )
-                autowatering_data.save()
-                channel_group_send(
-                    f"autowatering_data.{device.pk}",
-                    {"type": "update", "pk": autowatering_data.pk},
-                )
-                logger.debug(f"{device.name} {autowatering_data.time} 保存成功")
-        except Device.DoesNotExist:
-            logger.error(f"设备({device_name}) 不存在")
-
-
-def process_client_disconnected(event):
-    """处理设备下线事件"""
-    device_name = event["username"]
-    try:
-        device: Device = Device.objects.get(name=device_name)
-        device.is_online = False
-        device.offline_at = timezone.now()
-        device.save()
-        channel_group_send(f"device.{device.pk}", {"type": "update"})
-        logger.info(f"{device.name} 离线")
-    except Device.DoesNotExist:
-        logger.error(f"设备({device_name}) 不存在")
-
-
-def process_client_connected(event):
-    """处理设备上线事件"""
-    device_name = event["username"]
-    try:
-        device: Device = Device.objects.get(name=device_name)
-        device.is_online = True
-        device.online_at = timezone.now()
-        device.save()
-        channel_group_send(f"device.{device.pk}", {"type": "update"})
-        logger.info(f"{device.name} 在线")
-    except Device.DoesNotExist:
-        logger.error(f"设备({device_name}) 不存在")
-
-
-import base64
-
-from channels.db import database_sync_to_async
-from channels.generic.websocket import WebsocketConsumer
 
 
 @database_sync_to_async
@@ -161,7 +72,27 @@ class IotConsumer(WebsocketConsumer):
             logger.info(f"{device.name} 离线")
 
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-
-        self.send(text_data=json.dumps({"message": message}))
+        event = json.loads(text_data)
+        device: Device = self.scope["device"]
+        if device.device_type == "autowatering":
+            autowatering_data = AutowateringData(
+                device=device,
+                time=datetime.fromtimestamp(event["timestamp"], pytz.utc),
+                temperature=event["data"]["temperature"],
+                humidity=event["data"]["humidity"],
+                wifi_signal=event["data"]["wifi_signal"],
+                valve1=event["data"]["valve1"],
+                valve2=event["data"]["valve2"],
+                valve3=event["data"]["valve3"],
+                pump=event["data"]["pump"],
+                valve1_delay=event["data"]["valve1_delay"],
+                valve2_delay=event["data"]["valve2_delay"],
+                valve3_delay=event["data"]["valve3_delay"],
+                pump_delay=event["data"]["pump_delay"],
+            )
+            autowatering_data.save()
+            channel_group_send(
+                f"autowatering_data.{device.pk}",
+                {"type": "update", "pk": autowatering_data.pk},
+            )
+            logger.debug(f"{device.name} {autowatering_data.time} 保存成功")
