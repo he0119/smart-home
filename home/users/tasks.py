80f7143ef9ff482965ae294caeed605451c0b0da
hemengyang <hmy0119@gmail.com>
2022-10-10 10:53:36 +0000

    修复清除过期会话不可用的问题

--- home/users/tasks.py
+++ home/users/tasks.py
@@ -3,6 +3,6 @@ from django.core import management
 
 
 @shared_task
-def clear_expired_tokens():
-    """清楚过期的令牌"""
-    management.call_command("cleartokens", expired=True)
+def clear_sessions():
+    """清除过期的会话"""
+    management.call_command("clearsessions")
