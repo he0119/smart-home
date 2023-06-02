60e03179171b2dbebc0eec5f0ea988304d3da645
uy_sun <hmy0119@gmail.com>
2023-06-02 17:18:49 +0800

    更新依赖并修复 staticfiles 报错

--- home/settings/production.py
+++ home/settings/production.py
@@ -85,6 +85,9 @@ if os.getenv("AWS_S3_ENDPOINT_URL"):
         "default": {
             "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
         },
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        },
     }
 
     MEDIA_ROOT = ""
--- home/settings/settings.py
+++ home/settings/settings.py
@@ -254,6 +254,9 @@ STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.InMemoryStorage",
     },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
 }
 
 # AutoField
