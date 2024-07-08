7d0740253ac9bca085a74e1e9ace34dcc38d1afc
uy_sun <hmy0119@gmail.com>
2024-07-08 16:32:36 +0000

    chore(devcontainer): 简化脚本写法

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,10 +2,7 @@
 
 git config --global --add safe.directory /smart-home
 
-pipx install pre-commit
-
-poetry install
 pre-commit install --install-hooks
 
-poetry run python ./manage.py migrate
-poetry run python ./manage.py loaddata users board iot storage
+rye run migrate
+rye run loaddata users board iot storage
